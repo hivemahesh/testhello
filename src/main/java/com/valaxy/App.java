c3434a3bf6a50a3243c6268acc2479f17c881f9d
hivemahesh <mahesh.haruvaiah@gmail.com>
2020-10-14 11:09:13 +0000

    Third commit

--- src/main/java/com/valaxy/App.java
+++ src/main/java/com/valaxy/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!, Welcome to world of DevOps" );
-        System.out.println( "HI Roshini" );
+        System.out.println( "Hi Roshini" );
     }
 }
